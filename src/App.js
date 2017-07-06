685773b605b292feadbf987aae9a76ff731c04f4
Thomas Kieffer <thomaskieffer@web.de>
2017-07-06 22:25:42 +0200

    Some formatting

--- src/App.js
+++ src/App.js
@@ -19,26 +19,10 @@ import {
 var base64 = require('base-64');
 var utf8 = require('utf8');
 
-const Munich = () => (
-  <h1>MUNICH</h1>
-)
-
-const Passau = () => (
-  <h1>PASSAU</h1>
-)
 
 export default class App extends React.Component {
   constructor() {
     super();
-
-    moment.locale("de");
-
-    this.state = {
-      events: []
-    };
-
-    this.sendRequest = this.sendRequest.bind(this);
-
     // Add your tracking ID created from https://analytics.google.com/analytics/web/#home/
     ReactGA.initialize('UA-56053032-2');
     // This just needs to be called once since we have no routes in this case.
@@ -46,66 +30,32 @@ export default class App extends React.Component {
     ReactGA.pageview(window.location.pathname);
   }
 
-  sendRequest() {
-    var apigClientFactory = require('aws-api-gateway-client').default;
-
-    var params = {}
-    var additionalParams = {
-      queryParams: {
-        since:  moment().utc().format()
-      }
-    }
-    var pathTemplate = '/events/Munich'
-    var method = "POST"
-
-    var accessKey = process.env.REACT_APP_AWS_ACCESS_KEY
-    var secretKey = process.env.REACT_APP_AWS_SECRET_KEY
-
-    var apigClient = apigClientFactory.newClient({
-      invokeUrl: 'https://6milz2rjp1.execute-api.eu-central-1.amazonaws.com/prod',
-      accessKey: accessKey,
-      secretKey: secretKey,
-      region: 'eu-central-1'
-    });
-
-    // TODO add Promise
-    apigClient.invokeApi(params, pathTemplate, method, additionalParams)
-      .then(response => {
-        var events = response.data
-        console.log("Found " + events.length + " events");
-        console.log(events);
-        this.setState({
-          events: events
-        });
-      });
-  }
-
   render() {
     const meta = {
-      meta: {
-        name: "viewport",
-        content: "width=device-width, initial-scale=1.0"
-      }
+        meta: {
+            name: "viewport",
+            content: "width=device-width, initial-scale=1.0"
+        }
     };
 
-  return (
-      <Router history={browserHistory}>
-        <div id="controller">
-          <DocumentMeta {...meta} />
-          <div className="header">
-            <h1>I WANT TO GO OUT IN</h1>
-            <hr/>
-            <Route exact={true} path="/" render={() => (
-                <div>
-                    <Link to={'/munich'}><h3>Munich</h3></Link>
-                    <Link to={'/passau'}><h3>Passau</h3></Link>
-                </div>
-            )}/>
-            <Route path='/:city' component={EventList}/>
-          </div>
-        </div>
-      </Router>
-  );
+      return (
+          <Router history={browserHistory}>
+            <div id="controller">
+              <DocumentMeta {...meta} />
+              <div className="header">
+                <h1>I WANT TO GO OUT IN</h1>
+                <hr/>
+                <Route exact={true} path="/" render={() => (
+                    <div>
+                        <Link to={'/munich'}><h3>Munich</h3></Link>
+                        <Link to={'/passau'}><h3>Passau</h3></Link>
+                    </div>
+                )}/>
+                <Route path='/:city' component={EventList}/>
+              </div>
+            </div>
+          </Router>
+      );
   }
 }
 
@@ -153,7 +103,6 @@ class EventContainer extends React.Component {
           region: 'eu-central-1'
         });
 
-        // TODO add Promise
         apigClient.invokeApi(params, pathTemplate, method, additionalParams)
           .then(response => {
             var events = response.data
