a888305522820daad8eb3f875323cbc8d7f0940b
Thomas Kieffer <thomas.kieffer@codecentric.de>
2017-07-28 12:03:09 +0200

    Formatting

--- src/components/Event.js
+++ src/components/Event.js
@@ -3,84 +3,84 @@ import ReactModal from "react-modal";
 import moment from "moment";
 
 export default class Event extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      showModal: false
-    };
+    constructor() {
+        super();
+        this.state = {
+            showModal: false
+        };
 
-    this.handleOpenModal = this.handleOpenModal.bind(this);
-    this.handleCloseModal = this.handleCloseModal.bind(this);
-  }
+        this.handleOpenModal = this.handleOpenModal.bind(this);
+        this.handleCloseModal = this.handleCloseModal.bind(this);
+    }
 
-  handleOpenModal() {
-    this.setState({ showModal: true });
-  }
+    handleOpenModal() {
+        this.setState({showModal: true});
+    }
 
-  handleCloseModal() {
-    this.setState({ showModal: false });
-  }
+    handleCloseModal() {
+        this.setState({showModal: false});
+    }
 
-  render() {
-    return (
-      <div className="event" onClick={this.handleOpenModal}>
-        <div className="header">
-          <h3>{this.props.name}</h3>
-          <p className="startTime">
-            {this.props.startTime}
-          </p>
-          <p className="venue">
-            <b>{this.props.venue.name}</b><br />
-            {this.props.venue.street}<br />
-            {this.props.venue.zip}
-            ,
-            {" "}
-            {this.props.city}
-          </p>
-        </div>
-        <div className="description">
-          <img src={this.props.coverPicture} alt="coverPicture" />
-          <pre>
+    render() {
+        return (
+            <div className="event" onClick={this.handleOpenModal}>
+                <div className="header">
+                    <h3>{this.props.name}</h3>
+                    <p className="startTime">
+                        {this.props.startTime}
+                    </p>
+                    <p className="venue">
+                        <b>{this.props.venue.name}</b><br />
+                        {this.props.venue.street}<br />
+                        {this.props.venue.zip}
+                        ,
+                        {" "}
+                        {this.props.city}
+                    </p>
+                </div>
+                <div className="description">
+                    <img src={this.props.coverPicture} alt="coverPicture"/>
+                    <pre>
             {this.props.description}
           </pre>
-        </div>
+                </div>
 
-        <ReactModal
-          isOpen={this.state.showModal}
-          contentLabel="Event Details"
-          onRequestClose={this.handleCloseModal}
-          style={{
-            content: {
-              margin: "0 auto",
-              width: "50%",
-              "border-radius": "20px"
-            }
-          }}
-        >
-          <button onClick={this.handleCloseModal}>Close</button>
+                <ReactModal
+                    isOpen={this.state.showModal}
+                    contentLabel="Event Details"
+                    onRequestClose={this.handleCloseModal}
+                    style={{
+                        content: {
+                            margin: "0 auto",
+                            width: "50%",
+                            "border-radius": "20px"
+                        }
+                    }}
+                >
+                    <button onClick={this.handleCloseModal}>Close</button>
 
-          <div className="header">
-            <h3>{this.props.name}</h3>
-            <p className="startTime">
-              {moment(this.props.startTime).format("llll")}
-            </p>
-            <p className="venue">
-              {this.props.venue.name}<br />
-              {this.props.venue.street}<br />
-              {this.props.venue.postalcode}
-              ,
-              {" "}
-              {this.props.city}
-            </p>
-          </div>
-          <div className="description">
-            <img src={this.props.coverPicture} alt="coverPicture" />
-            <pre>
+                    <div className="header">
+                        <h3>{this.props.name}</h3>
+                        <p className="startTime">
+                            {moment(this.props.startTime).format("llll")}
+                        </p>
+                        <p className="venue">
+                            {this.props.venue.name}<br />
+                            {this.props.venue.street}<br />
+                            {this.props.venue.postalcode}
+                            ,
+                            {" "}
+                            {this.props.city}
+                        </p>
+                    </div>
+                    <div className="description">
+                        <img src={this.props.coverPicture} alt="coverPicture"/>
+                        <pre>
               {this.props.description}
             </pre>
-          </div>
-        </ReactModal>
-      </div>
-    );
-  }
+                    </div>
+                </ReactModal>
+            </div>
+        );
+    }
 }
