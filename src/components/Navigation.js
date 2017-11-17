cbb4ee1fba15c650ccf40b97098ef4b540954ffb
Thomas Kieffer <thomas.kieffer@codecentric.de>
2017-11-17 14:46:35 +0100

    Change font of city links

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,8 +3,8 @@ import {Link} from "react-router-dom";
 
 export const Navigation = () => (
     <div className={"center"}>
-        <Link to={'/munich'}><h3>Munich</h3></Link>
-        <Link to={'/passau'}><h3>Passau</h3></Link>
-        <Link to={'/regensburg'}><h3>Regensburg</h3></Link>
+        <Link to={'/munich'}><h3 className={"city-link"}>Munich</h3></Link>
+        <Link to={'/passau'}><h3 className={"city-link"}>Passau</h3></Link>
+        <Link to={'/regensburg'}><h3 className={"city-link"}>Regensburg</h3></Link>
     </div>
 );
