06a867d32580319ecef8e4f1d0416a52eeae4dbf
Thomas Kieffer <thomas.kieffer@codecentric.de>
2018-01-26 14:04:31 +0100

    Refactor modal dialogue

--- src/components/event/Event.js
+++ src/components/event/Event.js
@@ -2,68 +2,35 @@ import React from "react";
 import './Event.css';
 import {EventDescription} from "./EventDescription";
 import {EventHeader} from "./EventHeader";
-import Modal from 'react-modal';
 import moment from 'moment';
 import 'moment/locale/de';
+import {Modal} from "semantic-ui-react";
+import {Venue} from "./Venue";
 
-class Event extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            showModal: false
-        };
+export const Event = (props) => {
+    let name = props.name;
+    let startTime = moment(props.startTime).format('llll');
+    let venue = props.venue;
+    let description = props.description;
+    let imageUrl = props.imageUrl;
 
-        moment.locale('de');
+    moment.locale('de');
 
-        this.handleOpenModal = this.handleOpenModal.bind(this);
-        this.handleCloseModal = this.handleCloseModal.bind(this);
-    }
-
-    handleOpenModal() {
-        this.setState({showModal: true});
-    }
-
-    handleCloseModal() {
-        this.setState({showModal: false});
-    }
-
-    // TODO refactor
-    render() {
-        let name = this.props.name;
-        let startTime = moment(this.props.startTime).format('llll');
-        let venue = this.props.venue;
-        let description = this.props.description;
-        let imageUrl = this.props.imageUrl;
-
-        return (
-            <div className="event" onClick={this.handleOpenModal}>
+    return (
+        <div>
+            <Modal trigger={<div className="event">
                 <EventHeader name={name} startTime={startTime} venue={venue}/>
-                <EventDescription description={description} imageUrl={imageUrl}/>
-
-                <Modal
-                    isOpen={this.state.showModal}
-                    contentLabel="Event Details"
-                    onRequestClose={this.handleCloseModal}
-                    style={{
-                        content: {
-                            margin: "0 auto",
-                            // TODO adjust width of modal dialog on mobile vs desktop
-                            width: "60%",
-                            borderRadius: "20px"
-                        }
-                    }}
-                >
-                    <button onClick={this.handleCloseModal}>Close</button>
-
-                    <div className="modal-event">
-                        <EventHeader name={name} startTime={startTime} venue={venue}/>
+                <EventDescription imageUrl={imageUrl}/>
+            </div>} closeIcon>
+                <Modal.Header>{name}</Modal.Header>
+                <Modal.Content image scrolling>
+                    <Modal.Description>
+                        <p>{startTime}</p>
+                        <Venue {...venue} />
                         <EventDescription description={description} imageUrl={imageUrl}/>
-                    </div>
-                </Modal>
-            </div>
-        );
-
-    }
-}
-
-export default Event;
+                    </Modal.Description>
+                </Modal.Content>
+            </Modal>
+        </div>
+    );
+};
